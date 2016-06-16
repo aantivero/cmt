740cd4448a5b5edd8e33d6a15679812590ea611e
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-06-16 20:30:49 -0300

    Iniciando Firebase

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import {FIREBASE_PROVIDERS, defaultFirebase} from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  //Initialize Firebase app
+  defaultFirebase({
+    apiKey: "AIzaSyAVpIqYpetwJi3i87kKCwv72TEXBKDaa5Y",
+    authDomain: "project-3608165319188981417.firebaseapp.com",
+    databaseURL: "https://project-3608165319188981417.firebaseio.com",
+    storageBucket: "project-3608165319188981417.appspot.com",
+  })
+]);
 
